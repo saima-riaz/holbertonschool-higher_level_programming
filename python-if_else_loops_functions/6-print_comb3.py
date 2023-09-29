c4a0192883ae3452e87d7870593d9ab4f6d97bf3
saima-riaz <saimariaz1422@gmail.com>
2023-09-29 05:38:58 -0700

    code fixed

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-for x in range(0, 9):
+for x in range(0, 8):
     for y in range(x + 1, 10):
-        print("{:02d}".format(x * 10 +  y), end=", ")
+        print("{}{}".format(x, y), end=", ")
+print("89")

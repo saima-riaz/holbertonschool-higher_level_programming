7c809dfc42d54dd1db63ddea44588e2df5c65e33
saima-riaz <saimariaz1422@gmail.com>
2023-10-03 07:42:33 -0700

    fix error

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for x in reversed(my_list):
-        print("{:d}".format(x))
+    for x in range(len(my_list),0, -1):
+        print("{:d}".format(my_list[x-1]))

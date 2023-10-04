20c55a89c14c8b64c01356e940a7f2c66c4cb33e
saima-riaz <saimariaz1422@gmail.com>
2023-10-04 08:33:27 -0700

    fix space

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,9 @@
 
 def divisible_by_2(my_list=[]):
     result = []
-
     for x in my_list:
         if x % 2 == 0:
             result.append(True)
         else:
             result.append(False)
-    return result        
+    return result

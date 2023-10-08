72e0081287dbc2157cae603028baf73c0ec3b28c
saima-riaz <saimariaz1422@gmail.com>
2023-10-08 07:35:26 -0700

    fix error

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        return True
-    except ValueError:
-        return False
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+    except TypeError:
+        pass

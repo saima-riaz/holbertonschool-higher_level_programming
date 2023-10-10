bc1034ee49e56f9a028fa5e88526bddc0caa94c1
saima-riaz <saimariaz1422@gmail.com>
2023-10-10 03:12:26 -0700

    task 0 and Readme created

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
-"""
-define class square
-"""
-
+"""define class of a square"""
 
 class Square:
-    """
-    define empty square
-     """
-
-    def__init__(self):
-        """
-        initalizes an instance of the square class
-        """
-        pass
-
-    if__name__ == "__main__":
-        my_Square = Square()
+    """" define an empty class square"""

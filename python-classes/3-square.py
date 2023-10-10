3920aa9ef33f2ccaaca21ed5cb65b6a351d598f2
saima-riaz <saimariaz1422@gmail.com>
2023-10-10 08:25:53 -0700

    fix blankline

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,8 +11,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         """define square area"""
         return self.__size ** 2
-
-

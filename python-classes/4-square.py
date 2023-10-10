cc2ae31cfe2ef0bf6465c250e2670bac5c9eb73f
saima-riaz <saimariaz1422@gmail.com>
2023-10-10 13:27:27 -0700

    class square with function getter and setter

---
+++ python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''define an empty class'''
+
+
+class Square:
+    """class represent square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """square"""
+        return self.__size ** 2

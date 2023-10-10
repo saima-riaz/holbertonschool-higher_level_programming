21c7a800dab72ce637c907cc84cb3d8cf1202093
saima-riaz <saimariaz1422@gmail.com>
2023-10-10 08:23:35 -0700

    define class square and area

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """A class representing a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new Square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """define square area"""
+        return self.__size ** 2
+
+

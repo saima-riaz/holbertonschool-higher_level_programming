d15c8f514dcd4d25d8bfa95bfe3b430790d355b7
saima-riaz <saimariaz1422@gmail.com>
2023-10-10 07:25:07 -0700

    a fix error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,11 +6,14 @@
 
 class Square:
     def __int__(self, size=0):
+        self.__size = size
         """initalizes a new square"""
 
-        if not isinstance(size, int):
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            size.__size = size
+
+        def area(self):
+            """initalizes square area"""
+            return self.__size ** 2

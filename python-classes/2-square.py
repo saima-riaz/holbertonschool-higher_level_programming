189301e4f975120af45a6277c61fe7b7588a57ba
saima-riaz <saimariaz1422@gmail.com>
2023-10-10 07:48:09 -0700

    a fix pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,17 +5,12 @@
 
 
 class Square:
-    def area(self):
-            """initalizes square area"""
-            return self.__size ** 2
-
+""" define square"""
     def __int__(self, size=0):
-        """initalizes a new square"""
+        self.__size = size
+        """initalizes a 0 square"""
 
         if not isinstance(self, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
-        else:
-            self.__size = size

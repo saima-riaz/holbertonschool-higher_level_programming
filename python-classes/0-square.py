e0b6d08cfdef1a2b64f9f80634d2490d90fdbb1c
saima-riaz <saimariaz1422@gmail.com>
2023-10-10 02:10:59 -0700

    fix  error

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """
 define class square
-
 """
 
 
 class Square:
     """
     define empty square
-
-    """
+     """
 
     def__init__(self):
         """
         initalizes an instance of the square class
-
         """
 
         pass
 
     if__name__ == "__main__":
-        my_Square = Sqaare():
+        my_Square = Square():

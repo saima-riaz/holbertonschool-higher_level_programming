65de9b063b8c54af57955efc80997f97f7eaecd8
saima-riaz <saimariaz1422@gmail.com>
2023-10-12 10:27:05 -0700

    fix code error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,9 +3,10 @@
 
 def add_integer(a, b=98):
     """function that add two intergers"""
-    if not isinstance(a, int, float):
-    if not isinstance(b, int, float):
-        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
         """If 'b' is a float, cast it to an integer"""
     
-    return int(a + b)
+    return int(a) +int(b)

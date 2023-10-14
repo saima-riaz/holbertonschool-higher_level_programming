417b2ebf6d1ab31793b2ba19586293756fa088a9
saima-riaz <saimariaz1422@gmail.com>
2023-10-14 15:08:58 -0700

    task Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError("first_name must be a string")
+    print("My_name is {} {}".format(first_name, last_name))

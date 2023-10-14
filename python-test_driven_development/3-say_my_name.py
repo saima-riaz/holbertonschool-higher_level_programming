fe58f6aa2c699841e97fcc82c06e9b6db6b6b64f
saima-riaz <saimariaz1422@gmail.com>
2023-10-14 15:38:12 -0700

    fixed error

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" print my_name is <first name> <last name> otherwise raise typeerror exception"""
+
 
 def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name must be a string or last_name must be a string")
-    else:
-        full_name = f"My name is {first_name} {last_name}".strip()
-        print(full_name)
+    """ valid first_name"""
+    if type(first_name) is not str:
+            raise TypeError("first_name must be a string")
+    if type(last_name)is not str:
+        raise TypeError("last_name must be a string")
+    print("My_name is {} {}".format(first_name, last_name))

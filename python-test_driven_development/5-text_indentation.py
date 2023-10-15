a50eefa4250af528753c6ccec7b101a1194ae6ad
saima-riaz <saimariaz1422@gmail.com>
2023-10-15 07:03:53 -0700

    fix test and code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+""""
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    otherrwise raise typeerror exception"""
+
+
 def text_indentation(text):
+    """ validation """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    """ create a list of my separators and copy text in new text """
     separators = ('.', '?', ':')
     current_line = ""
 

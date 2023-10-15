e86be6ea408a0e0dd6ec6c8191668ea7e073ea89
saima-riaz <saimariaz1422@gmail.com>
2023-10-15 04:07:56 -0700

    added documention

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+""" Print a square made of '#' characters with the specified size"""
+
 
 def print_square(size):
+
     if not isinstance(size, int):
+        """Raises:
+    TypeError: If 'size' is not an integer
+    ValueError: If 'size' is less than 0"""
+
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError(" size must be >= 0")

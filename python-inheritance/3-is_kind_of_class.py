8b2999b8cb4b7593378384b13092d533733ca49b
saima-riaz <saimariaz1422@gmail.com>
2023-10-18 06:47:07 -0700

    object is an instance of a class that inherited from,

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""check obj is instance and specified class that inherited from
+return True,otherwise False"""
+
+
+def is_kind_of_class(obj, a_class):
+    """mention class"""
+    return isinstance(obj, a_class)

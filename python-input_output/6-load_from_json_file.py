5c606d580718783bf2c5b106c981ab6450c03c82
saima-riaz <saimariaz1422@gmail.com>
2023-10-20 06:43:30 -0700

    fuction that load an obj using json

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,7 @@ import json
 
 def load_from_json_file(filename):
     """load an object using json"""
+
     with open(filename, "r") as file:
         x = json.load(file)
-    return x
+        return x

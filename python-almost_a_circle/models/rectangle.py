1fba531a738be348040b8e90745740669b686815
saima-riaz <saimariaz1422@gmail.com>
2023-10-24 09:30:51 -0700

    fix error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-""" first class base """
+""" import modles """
 
-
-class Base:
-    """ base class """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
+from models.base import Base
+""" define class """
 
 class Rectangle(Base):
     """ child class """

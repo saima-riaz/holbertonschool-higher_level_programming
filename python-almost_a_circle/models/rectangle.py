019cd1d75070b1d4ebf5a0a229f65ff02e17a32a
saima-riaz <saimariaz1422@gmail.com>
2023-10-24 09:06:10 -0700

    fix error

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,16 +15,25 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-
 class Rectangle(Base):
-    """ define class childi Rectangle """
+    """ child class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor for the Rectangle class.
 
-        """ Class constructor """
+        Args:
+        width: Width of the rectangle.
+        height: Height of the rectangle.
+        x: X-coordinate of the rectangle's position (default is 0).
+        y: Y-coordinate of the rectangle's position (default is 0).
+        id:optional identifier.If not provided,a unique id will be assigned.
+
+        constructor of the parent cls(Base)called using super().__init__(id).
+        """
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        """ called super class """
         super().__init__(id)
+

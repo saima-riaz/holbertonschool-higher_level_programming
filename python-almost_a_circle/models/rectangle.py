64e0c6b9c34d17b4be47bc6269c2ff155b1ebd8e
saima-riaz <saimariaz1422@gmail.com>
2023-10-24 08:49:55 -0700

    added task 2. First Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+class Base:
+    """define class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+class Rectangle(Base):
+    """define class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)

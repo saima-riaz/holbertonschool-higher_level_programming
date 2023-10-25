6141f6993e966ab13822086519d3093e02175651
saima-riaz <saimariaz1422@gmail.com>
2023-10-25 07:32:12 -0700

    fix error

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -13,3 +13,28 @@ class Square(Rectangle):
     def __str__(self):
         """ define Square """
         return f"[Square] ({self.id}), {self.x}/{self.y} - {self.width}"
+    
+    @property
+    def size(self):
+        return(self.__width)
+
+    @size.setter
+    def size(self, value):
+        """ size """
+        self.__width = value
+        self.__height = value
+
+    def update(self, *args, **kwargs):
+        """ update attribut"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)

417f4b40127f9973babdbf0dd9bc3b5197364f8c
saima-riaz <saimariaz1422@gmail.com>
2023-10-25 07:40:09 -0700

    fix error

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,28 +4,28 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ define class """
+    """def class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, id, x, y, id)
+        super().__init__(size, size, x, y, id)
         self.size = size
 
     def __str__(self):
-        """ define Square """
-        return f"[Square] ({self.id}), {self.x}/{self.y} - {self.width}"
-    
+        """Square"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
     @property
     def size(self):
-        return(self.width)
+        return self.width
 
     @size.setter
     def size(self, value):
-        """ size """
+        """size"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """ update attribut"""
+        """def update attribut"""
         if args:
 
             if len(args) >= 1:

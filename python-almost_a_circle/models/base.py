a727a6501a02dfcfee38c2054df910d2897065d4
saima-riaz <saimariaz1422@gmail.com>
2023-10-26 13:56:50 -0700

    fix pycodestyle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """ Save a list of objects to a file in JSON format """

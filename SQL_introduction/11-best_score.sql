7b2f84cfb217bdef52e4947430f8b9202e3e54ed
saima-riaz <saimariaz1422@gmail.com>
2023-11-22 05:19:28 -0800

    updated code

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,1 +1,2 @@
-SELECT DISTINCT score, name FROM hbtn_0c_0.second_table WHERE score >= 10 ORDER BY score DESC;
+-- that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT DISTINCT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

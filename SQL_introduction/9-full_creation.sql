05f79f1ed2dbb5548d0af7289ce8a1105e6e3a1d
saima-riaz <saimariaz1422@gmail.com>
2023-11-22 05:10:34 -0800

    fix name error

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
     id INT,
     name VARCHAR(256),

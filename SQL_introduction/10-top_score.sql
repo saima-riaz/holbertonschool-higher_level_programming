35aca78c5675da43dca54460e360d52ad4c31798
saima-riaz <saimariaz1422@gmail.com>
2023-11-22 05:17:35 -0800

    script that records the second table

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
---lists all records of the table second_table of the database hbtb_0c_0 in your MySQL server
-SELECT DISTINCT score, name FROM second_table ORDER BY score DESC;
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT DISTINCT score, name
+FROM second_table order by score desc;

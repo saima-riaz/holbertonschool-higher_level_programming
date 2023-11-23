d9722f8fa8e14e9f9dbaa0a72eadc307e056df6a
saima-riaz <saimariaz1422@gmail.com>
2023-11-23 04:23:42 -0800

    fix error

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user if not exists
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'users_0d_2_pwd';
 -- Grant SELECT privilege on the specific database
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

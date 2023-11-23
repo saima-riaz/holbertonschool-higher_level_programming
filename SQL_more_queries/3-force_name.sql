9437036d8a65d7ef774fb9eb2bfce4dbcee47bce
saima-riaz <saimariaz1422@gmail.com>
2023-11-23 05:48:12 -0800

    fix error

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create table if not exists
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.force_name (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)

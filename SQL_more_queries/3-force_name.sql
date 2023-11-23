b86d45e8870925124bdd39c7d8523f28c01c6e20
saima-riaz <saimariaz1422@gmail.com>
2023-11-23 05:49:51 -0800

    fix error

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
--- Create table if not exists
+-- creates the table force_name on your MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
 );

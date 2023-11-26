cc9bf61e8c6a571b61163eea5fe34523f387da03
saima-riaz <saimariaz1422@gmail.com>
2023-11-26 04:23:42 -0800

    list of show database without genre linked

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- Select the title of TV shows and genre_id from the specified tables
+-- Use a LEFT JOIN to include all records from tv_shows even if there is no matching record in tv_show_genres
+-- Join the tables on the show_id column in tv_show_genres and the id column in tv_shows
+-- Include only the records where there is no matching genre (genre_id is NULL)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;

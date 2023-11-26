c24972f02ecdacbf3eeb446e89954ce1e9eca737
saima-riaz <saimariaz1422@gmail.com>
2023-11-26 04:48:05 -0800

    list of all comedy show in the database

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- Select the titles of shows from tv_shows where the genre is Comedy
+-- Join tables to link genres, show genres, and shows
+-- Filter for the genre Comedy
+-- Order results in ascending order by show title
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
